2f0d8a873261c7b39829b3a8d3f176ea021e5988
Heng Tse Chou <legohank1219@gmail.com>
2023-07-12 14:22:32 +0800

    add: add banner, icon create login page, login route

--- website/blog/routes.py
+++ website/blog/routes.py
@@ -1,9 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 blog = Blueprint('blog', __name__, template_folder='../templates/blog/')
 
 @blog.route('/', methods = ['GET'])
 def home():
 
-    return render_template('home.html')
+    return render_template('home.html', nav=True)
+
+@blog.route('/login', methods = ['GET', 'POST'])
+def login():
+
+    if request.method == 'GET':
+        return render_template('login.html', nav=False)
+
+    # find user in db
+
+    # login user
 
