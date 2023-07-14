9b346072ebd084acbc12f4ef2310090b2c8bdbb7
Heng Tse Chou <legohank1219@gmail.com>
2023-07-14 12:18:33 +0800

    register logic; validate on register page

--- website/blog/routes.py
+++ website/blog/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+import bcrypt
+from website.extensions.db import db_users
 
 blog = Blueprint('blog', __name__, template_folder='../templates/blog/')
 
@@ -22,5 +24,25 @@ def login():
 @blog.route('/register', methods = ['GET', 'POST'])
 def register():
 
+    if request.method == 'GET':
+        return render_template('register.html')
+    
+    # registeration
+    # check if user exists
+    data = request.form.to_dict()    
+    if db_users.exists('username', data['username']):
+        flash('Username already exists. Please try another one.', category='error')
+        return render_template('register.html')
+    # create user in db
 
-    return render_template('register.html', nav=False)
+    hashed_pw = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt(12))
+    hashed_pw = hashed_pw.decode('utf-8')
+    data['password'] = hashed_pw
+    del data['terms']
+    db_users.create_user(data)
+
+    # succeeded and return to login page
+    flash('Registeration succeeded.', category='success')    
+    return redirect(url_for('blog.login'))
+    
+
--- website/extensions/db.py
+++ website/extensions/db.py
@@ -1,2 +1,37 @@
 import pymongo
 from website.config import MONGO_URL
+
+class Database(object):
+    def __init__(self):
+
+        client = pymongo.MongoClient(MONGO_URL, connect=False)
+        self.db = client['blog']
+
+class DB_Users(Database):
+
+    def __init__(self):
+        super().__init__()
+        self.collection = self.db['users']
+
+    def exists(self, key, value):
+
+        exists = self.collection.find_one({key: value})
+        if exists:
+            return True
+        return False
+    
+    def create_user(self, data):
+        
+        x = self.collection.insert_one(data)
+        return x
+
+class DB_Posts(Database):
+
+    def __init__(self):
+        super().__init__()
+        self.collections = self.db['posts']
+
+
+
+db_users = DB_Users()
+db_posts = DB_Posts()
