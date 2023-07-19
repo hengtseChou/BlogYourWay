7b6b4cfbe2c1571ca70a6829dde1a0d56d6b95b5
Heng Tse Chou <legohank1219@gmail.com>
2023-07-19 14:43:48 +0800

    add: post featuring, archive and delete

--- website/backstage/routes.py
+++ website/backstage/routes.py
@@ -14,6 +14,8 @@ def panel():
 
     return render_template('panel.html')
 
+### tabs
+
 @backstage.route('/posts', methods=['GET', 'POST'])
 @login_required
 def post_control():
@@ -25,10 +27,10 @@ def post_control():
         # create a new post in db
         new_post = request.form.to_dict()
         # set posting time  
-        if ENV == 'debug':
-            new_post['created_at'] = datetime.now()
+        if ENV == 'debug':            
+            new_post['created_at'] = new_post['last_modified'] = datetime.now()
         elif ENV == 'prod':
-            new_post['created_at'] = datetime.now() + timedelta(hours=8)
+            new_post['created_at'] = new_post['last_modified'] = datetime.now() + timedelta(hours=8)
         # set other attributes
         author = db_users.find_via('username', current_user.username)
         new_post['author'] = current_user.username
@@ -37,7 +39,7 @@ def post_control():
         db_users.update_values(current_user.username, 'posts_count', current_user.posts_count + 1)
         new_post['clicks'] = 0
         new_post['comments'] = 0
-        new_post['archieved'] = False
+        new_post['archived'] = False
         new_post['featured'] = False
         # uid is used to link posts and comments
         uid = str(uuid4())
@@ -57,23 +59,101 @@ def post_control():
     if page == 1:
         posts = db_posts.collection.find({
             'author': current_user.username,
-            'archieved': False
+            'archived': False
         }).limit(20).sort('created_at', -1) # descending: newest
     elif page > 1:
         posts = db_posts.collection.find({
             'author': current_user.username,
-            'archieved': False
+            'archived': False
         }).skip(min(page*20, max_skip)).limit(20).sort('created_at', -1)
 
 
     return render_template('posts.html', posts=posts)
-    
+
+@backstage.route('/archive', methods=['GET'])
+@login_required
+def archive_control():
+
+    session['at'] = 'archive'
+
+    # query through posts
+    posts = db_posts.collection.find({
+            'author': current_user.username,
+            'archived': True
+    }).sort('created_at', -1) # descending: newest
+
+    return render_template('archive.html', posts=posts)
 
 @backstage.route('/theme', methods=['GET', 'POST'])
 @login_required
 def theme():
 
     return render_template('theme.html')
+
+
+### actions
+
+@backstage.route('/edit-featured', methods=['GET'])
+@login_required
+def edit_featured():
+
+    if session['at'] == 'posts':
+
+        if request.args.get('featured') == 'to_true':
+            featured_status_new = True
+        else:
+            featured_status_new = False
+        post_uid = request.args.get('uid')
+
+        db_posts.collection.update_one(filter={'uid': post_uid}, 
+                                    update={'$set': {'featured': featured_status_new}})
+    
+    else: 
+        flash('Access Denied. ', category='error')
+    return redirect(url_for('backstage.post_control'))
+
+@backstage.route('/edit-archived', methods=['GET'])
+@login_required
+def edit_archived():
+
+    if session['at'] == 'posts':        
+        post_uid = request.args.get('uid')
+
+        db_posts.collection.update_one(filter={'uid': post_uid}, 
+                                    update={'$set': {'archived': True}})
+        return redirect(url_for('backstage.post_control'))
+    
+    elif session['at'] == 'archive':
+        post_uid = request.args.get('uid')
+
+        db_posts.collection.update_one(filter={'uid': post_uid}, 
+                                    update={'$set': {'archived': False}})
+        return redirect(url_for('backstage.archive_control'))
+
+    
+    flash('Access Denied. ', category='error')
+    return redirect(url_for('backstage.archive_control'))
+    
+@backstage.route('/delete', methods=['GET'])
+@login_required
+def delete():
+
+    target = request.args.get('type')
+    uid = request.args.get('uid')
+
+    if session['at'] == 'archive':
+
+        if target == 'post':
+            db_posts.collection.delete_one({'uid': uid})
+            flash('Post deleted!', category='success')
+            return redirect(url_for('backstage.archive_control'))
+        elif target == 'work':
+            # delete work
+            return redirect(url_for('backstage.archive_control'))
+    
+    flash('Access Denied. ', category='error')
+    return redirect(url_for('backstage.archive_control'))
+
 
 @backstage.route('/logout', methods=['GET'])
 @login_required
