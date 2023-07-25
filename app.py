aa3da83bf53abc241bf471fb94d9f1443651c8bc
Heng Tse Chou <legohank1219@gmail.com>
2023-07-25 10:20:56 +0800

    add port=5000

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ app = create_app()
 
 if __name__ == '__main__':
     if DEBUG:
-        app.run(debug=True)
+        app.run(debug=True, port=5000)
     else:
-        app.run()
+        app.run(port=5000)
