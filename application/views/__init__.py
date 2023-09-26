e2177cdc2d5e57ad35bc7300011030b32bba8b4d
Hank Chou <hengtse.me@gmail.com>
2023-09-26 13:05:07 +0800

    rename testing modules

--- application/views/__init__.py
+++ application/views/__init__.py
@@ -1,7 +1,2 @@
-from application.views.blog import blog
-from application.views.backstage import backstage
-
-# rename blueprints and put them here for easy importing
-
-blog_bp = blog
-backstage_bp = backstage
+from .blog import blog as blog_bp
+from .backstage import backstage as backstage_bp
