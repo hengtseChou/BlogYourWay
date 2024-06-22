fd1f4aa70cfd08b4cf2d9b3741d75c65acebda2f
hengtseChou <hengtse.me@gmail.com>
2024-06-23 00:14:04 +0800

    add a confirm deletion modal for articles

--- blogyourway/static/js/backstage/archive.js
+++ blogyourway/static/js/backstage/archive.js
@@ -1,2 +1,13 @@
 const mobileNavBtn = document.getElementById("mobile-nav-archive");
 mobileNavBtn.style.color = "white";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const deleteButtons = document.querySelectorAll(".delete-article-btn");
+  const deleteModalLink = document.getElementById("deleteArticleBtn");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const articleUid = this.dataset.articleUid;
+      deleteModalLink.href = `/backstage/delete/article?&uid=${articleUid}`; // Update the modal's delete link
+    });
+  });
+});
