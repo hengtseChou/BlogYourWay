069a3ee14af81e5e0148d04b3fbb99ff37b7a17e
hengtseChou <hengtse.me@gmail.com>
2024-06-22 19:14:13 +0800

    isolate the scripts in blog templates

---
+++ blogyourway/static/js/article.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".ajax-profile-pic").forEach(fetchReplacementImage);
+  setTimeout(sendReadCountRequest, 30000);
+  document
+    .getElementById("comment-form")
+    .addEventListener("keypress", preventFormEnter);
+});
+
+function fetchReplacementImage(imgElement) {
+  const originalSrc = imgElement.getAttribute("src");
+  fetch(originalSrc)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.imageUrl) {
+        imgElement.setAttribute("src", data.imageUrl);
+      } else {
+        console.error("Image URL not found in the response.");
+      }
+    })
+    .catch((error) =>
+      console.error(`Error fetching replacement image: ${error}`),
+    );
+}
+
+function sendReadCountRequest() {
+  fetch("/readcount-increment?post_uid={{ post.post_uid }}").catch((error) =>
+    console.error(`Error incrementing read count: ${error}`),
+  );
+}
+
+function preventFormEnter(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+  }
+}
---
+++ blogyourway/static/js/base.js
@@ -0,0 +1,29 @@
+hljs.highlightAll();
+
+// notification (toast) from flask
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    var notification = document.getElementById("notification");
+    if (notification) {
+      notification.style.opacity = "0";
+      setTimeout(function () {
+        notification.style.display = "none";
+      }, 300);
+    }
+  }, 3000);
+});
+
+// update year tag in the footer
+function updateCurrentYear() {
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+  var yearTag = document.getElementById("yearTag");
+  if (yearTag) {
+    yearTag.innerHTML = currentYear;
+  }
+  var yearTagMobile = document.getElementById("yearTagMobile");
+  if (yearTagMobile) {
+    yearTagMobile.innerHTML = currentYear;
+  }
+}
+updateCurrentYear();
