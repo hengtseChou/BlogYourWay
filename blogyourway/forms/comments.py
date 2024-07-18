7f5a699220bb9e9e8756450ce7898394cb8ab470
hengtseChou <hengtse.me@gmail.com>
2024-07-18 17:31:41 +0800

    data required -> input required

--- blogyourway/forms/comments.py
+++ blogyourway/forms/comments.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email, Optional
+from wtforms.validators import Email, InputRequired, Optional
 
 from blogyourway.config import RECAPTCHA_KEY
 
 
 class CommentForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired()])
+    name = StringField("Name", validators=[InputRequired()])
     email = StringField("Email", validators=[Email(), Optional()])
-    comment = TextAreaField("Comment", validators=[DataRequired()])
+    comment = TextAreaField("Comment", validators=[InputRequired()])
     submit_ = SubmitField(
         "Submit", render_kw={"data-sitekey": RECAPTCHA_KEY, "data-callback": "onSubmit"}
     )
--- blogyourway/forms/posts.py
+++ blogyourway/forms/posts.py
@@ -2,36 +2,38 @@ import re
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import URL, DataRequired, Regexp
+from wtforms.validators import URL, InputRequired, Optional, Regexp
 
-slug_pattern = re.compile(r"^[a-z0-9]+(?:-[a-z0-9]+)*$|^$")
+slug_pattern = re.compile(r"^[a-z0-9]+(?:-[a-z0-9]+)*$")
 
 
 class NewPostForm(FlaskForm):
-    title = StringField(validators=[DataRequired()])
-    subtitle = StringField(validators=[DataRequired()])
+    title = StringField(validators=[InputRequired()])
+    subtitle = StringField(validators=[InputRequired()])
     tags = StringField(
-        render_kw={"placeholder": "Separate tags with ','"}, validators=[DataRequired()]
+        render_kw={"placeholder": "Separate tags with ','"}, validators=[InputRequired()]
     )
-    cover_url = StringField(render_kw={"placeholder": "Insert image url here"}, validators=[URL()])
+    cover_url = StringField(
+        render_kw={"placeholder": "Insert image url"}, validators=[Optional(), URL()]
+    )
     custom_slug = StringField(
         render_kw={"placeholder": "Must be an url-friendly string"},
-        validators=[Regexp(slug_pattern)],
+        validators=[Optional(), Regexp(slug_pattern)],
     )
     editor = TextAreaField()
     submit_ = SubmitField(label="Submit")
 
 
 class EditPostForm(FlaskForm):
-    title = StringField(validators=[DataRequired()])
-    subtitle = StringField(validators=[DataRequired()])
+    title = StringField(validators=[InputRequired()])
+    subtitle = StringField(validators=[InputRequired()])
     tags = StringField(
-        render_kw={"placeholder": "Separate tags with ','"}, validators=[DataRequired()]
+        render_kw={"placeholder": "Separate tags with ','"}, validators=[InputRequired()]
     )
-    cover_url = StringField(render_kw={"placeholder": "Insert image url here"}, validators=[URL()])
+    cover_url = StringField(render_kw={"placeholder": "Insert image url"}, validators=[URL()])
     custom_slug = StringField(
         render_kw={"placeholder": "Must be an url-friendly string"},
-        validators=[Regexp(slug_pattern)],
+        validators=[Optional(), Regexp(slug_pattern)],
     )
     editor = TextAreaField()
     submit_ = SubmitField(label="Save Changes", render_kw={"onclick": "return validateUpdate()"})
